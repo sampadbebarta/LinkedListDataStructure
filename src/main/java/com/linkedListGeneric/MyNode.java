39c9c00826a5b5ded08c82c0fadaf07abc51b189
Sampad <bebartasampad@gmail.com>
2021-03-30 20:07:26 +0530

    Linked List by Appending

--- src/main/java/com/linkedListGeneric/MyNode.java
+++ src/main/java/com/linkedListGeneric/MyNode.java
@@ -1,29 +1,30 @@
 package com.linkedListGeneric;
-//create linked list by adding 30,56,70
-public class MyNode<K> implements INode<K> {
-	private K Key;
-	private INode next;
 
-	public MyNode(K key) {
-		this.Key = key;
-		this.next = null;
-	}
+// linked list by appending
 
-	@Override
-	public K getKey() {
-		return Key;
-	}
+public class MyNode <K>implements INode<K>{
+    private  K Key;
+    private  INode next;
+    public  MyNode(K key){
+        this.Key = key;
+        this.next = null;
+    }
 
-	@Override
-	public void setKey(K key) {
-		this.Key = key;
-	}
+    @Override
+    public K getKey() {
+        return Key;
+    }
 
-	public INode getNext() {
-		return next;
-	}
+    @Override
+    public void setKey(K key) {
+        this.Key=key;
+    }
 
-	public void setNext(INode next) {
-		this.next = next;
-	}
+    public INode getNext() {
+        return next;
+    }
+
+    public void setNext(INode next) {
+        this.next = next;
+    }
 }
--- src/test/java/com/linkedListGeneric/MyNodeTest.java
+++ src/test/java/com/linkedListGeneric/MyNodeTest.java
@@ -1,5 +1,6 @@
 package com.linkedListGeneric;
-//create linked list by adding 30,56,70
+
+// linked list by appending
 import org.junit.Assert;
 import org.junit.Test;
 
