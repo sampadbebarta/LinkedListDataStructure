1c0704c05573fdd05fbc3c7374d5de2f0e941840
Sampad <bebartasampad@gmail.com>
2021-03-30 20:21:34 +0530

    Delete the first element in Linked List

--- src/main/java/com/linkedListGeneric/MyNode.java
+++ src/main/java/com/linkedListGeneric/MyNode.java
@@ -1,30 +1,29 @@
 package com.linkedListGeneric;
 
-// linked list by appending
+public class MyNode<K> implements INode<K> {
+	private K Key;
+	private INode next;
 
-public class MyNode <K>implements INode<K>{
-    private  K Key;
-    private  INode next;
-    public  MyNode(K key){
-        this.Key = key;
-        this.next = null;
-    }
+	public MyNode(K key) {
+		this.Key = key;
+		this.next = null;
+	}
 
-    @Override
-    public K getKey() {
-        return Key;
-    }
+	@Override
+	public K getKey() {
+		return Key;
+	}
 
-    @Override
-    public void setKey(K key) {
-        this.Key=key;
-    }
+	@Override
+	public void setKey(K key) {
+		this.Key = key;
+	}
 
-    public INode getNext() {
-        return next;
-    }
+	public INode getNext() {
+		return next;
+	}
 
-    public void setNext(INode next) {
-        this.next = next;
-    }
+	public void setNext(INode next) {
+		this.next = next;
+	}
 }
--- src/test/java/com/linkedListGeneric/MyNodeTest.java
+++ src/test/java/com/linkedListGeneric/MyNodeTest.java
@@ -1,6 +1,5 @@
 package com.linkedListGeneric;
 
-// linked list by appending
 import org.junit.Assert;
 import org.junit.Test;
 
